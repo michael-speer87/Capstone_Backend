7799be417964bbabe1f60ca5a7893483c06db8e7
DavidDeSal <tylerdesalvo23@gmail.com>
2025-10-05 17:21:22 -0500

    Permissions changed to IsAuthenticated.

--- vendors/views.py
+++ vendors/views.py
@@ -5,6 +5,7 @@ from .serializers import VendorCreateSerializer
 from .models import Vendor
 
 class VendorCreateView(generics.CreateAPIView):
-    permission_classes = [permissions.AllowAny]  # switch to IsAuthenticated when you add auth tokens
+    permission_classes = [permissions.IsAuthenticated]  
     serializer_class = VendorCreateSerializer
     queryset = Vendor.objects.all()
+
