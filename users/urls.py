a1406ebccd4d63019549cd2778784613865ca59d
DavidDeSal <tylerdesalvo23@gmail.com>
2025-10-12 16:06:08 -0500

    Added endpoints for password reset/confirm

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,7 @@ from .views import RegisterView
 
 urlpatterns = [
     path("", RegisterView.as_view(), name="auth_register"),
+    path("password-reset/", PasswordResetRequestView.as_view(), name="password_reset"),
+    path("password-reset-confirm/", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
 ]
+
