255abb4c652ab604c68457f6ca6627c524f5abdc
DavidDeSal <tylerdesalvo23@gmail.com>
2025-11-05 22:22:44 -0600

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -17,14 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views_auth import CookieTokenObtainPairView, CookieTokenRefreshView, LogoutView, RoleProfileView
+from .views_auth import CookieTokenObtainPairView, CookieTokenRefreshView, LogoutView, RoleProfileView, MyTokenObtainPairView
 from users.views import PasswordResetRequestView, PasswordResetConfirmView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/auth/", include("users.urls")),
     path("api/profile/", RoleProfileView.as_view(), name="profile-create"),
-    path("api/auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/auth/token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/auth/login/", CookieTokenObtainPairView.as_view(), name="cookie_login"),
     path("api/auth/refresh-cookie/", CookieTokenRefreshView.as_view(), name="cookie_refresh"),
@@ -32,3 +32,4 @@ urlpatterns = [
     path("api/auth/password-reset/", PasswordResetRequestView.as_view(), name="password_reset"),
     path("api/auth/password-reset-confirm/", PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
 ]
+
